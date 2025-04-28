544f9903cf2f4039ddd8f2a26fb9fc9c56087731
Loay Sherif <loaysherif40@gmail.com>
2025-04-28 05:25:39 +0300

    employee_service adjusted

--- lib/services/employee_service.dart
+++ lib/services/employee_service.dart
@@ -1,21 +1,22 @@
 import 'dart:convert';
-import 'package:http/http.dart' as http;
+import 'package:dio/dio.dart';
 import '../models/employee_model.dart';
 
-/// EmployeeService handles API calls related to Employee data.
 class EmployeeService {
+  final Dio _dio = Dio();
   final String _baseUrl = 'http://dummy.restapiexample.com/api/v1/employees';
 
-  /// Fetch employees from the API and parse the response into a list of EmployeeModel.
   Future<List<EmployeeModel>> fetchEmployees() async {
-    final response = await http.get(Uri.parse(_baseUrl));
-
-    if (response.statusCode == 200) {
-      final decoded = jsonDecode(response.body);
-      final List<dynamic> employeeList = decoded['data'];
-      return employeeList.map((e) => EmployeeModel.fromJson(e)).toList();
-    } else {
-      throw Exception('Failed to load employees');
+    try {
+      final response = await _dio.get(_baseUrl);
+      if (response.statusCode == 200) {
+        final List<dynamic> employeeList = jsonDecode(response.data)['data'];
+        return employeeList.map((e) => EmployeeModel.fromJson(e)).toList();
+      } else {
+        throw Exception('Failed to load employees');
+      }
+    } catch (e) {
+      throw Exception('Error occurred: $e');
     }
   }
 }
